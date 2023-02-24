d088b414995ad934bc3c76ed461b0d3b499d9fc3
annielayton <125421402+annielayton@users.noreply.github.com>
2023-02-24 08:16:09 -0700

    added api url and api key

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-alert("Hello");
+let apiKey = "9t79b0co34f46fb4a0da6a3fdcc9e54d";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query={New York}&key={9t79b0co34f46fb4a0da6a3fdcc9e54d}";
+
+console.log(apiUrl);
