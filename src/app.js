c4cd06bff6d09b7081488f4809b58dc7da7cbf45
annielayton <125421402+annielayton@users.noreply.github.com>
2023-02-27 07:18:34 -0700

    updated js with suggested code to make console log work

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 let apiKey = "9t79b0co34f46fb4a0da6a3fdcc9e54d";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={New York}&key={9t79b0co34f46fb4a0da6a3fdcc9e54d}";
+let city = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 console.log(apiUrl);
