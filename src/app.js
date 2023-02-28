352474b0f54e70ec9ff296d4b147fe6927e54709
annielayton <125421402+annielayton@users.noreply.github.com>
2023-02-27 19:40:14 -0700

    put in js for city, temperature, wind, and humidity

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-let apiKey = "9t79b0co34f46fb4a0da6a3fdcc9e54d";
-let city = "New York";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+let city = "London";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
